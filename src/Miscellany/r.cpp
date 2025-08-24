d096e19580c67873fd6666d6d8a34004a598b612
panyuzhou666 <3285791820@qq.com>
2025-08-24 14:59:17 +0800

    更新 purfer 和运行脚本

--- src/Miscellany/r.cpp
+++ src/Miscellany/r.cpp
@@ -6,23 +6,32 @@ int main(int argc, char* argv[]) {
     string cpp = s + ".cpp";
     string exe = s + ".exe";
     string out = s + ".out";
+    string err = s + ".err";
 
     string cp = "g++ -std=c++20 -O2 " + cpp + " -o " + exe;
+
     if (system(cp.c_str()) != 0) {
         cerr << "Compile Failed!" << endl;
         return 1;
+    } else {
+        system("cls");
+        cerr << "Compile Success!" << endl;
     }
 
     string run = exe;
     if (argc > 2) {
         run += " < " + string(argv[2]);
     }
-    run += " > " + out;
+    run += " > " + out + " 2> " + err;
 
     system(run.c_str());
 
     cout << "\n--- Output ---\n";
     system(("type " + out).c_str());
+    cout << "\n--------------\n";
+
+    cout << "\n--- Error ---\n";
+    system(("type " + err).c_str());
     cout << "\n--------------\n";
 
     return 0;
