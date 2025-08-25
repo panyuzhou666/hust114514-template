738bae6a1d7d67b450f091e85a170f7b0e1c0a85
panyuzhou666 <3285791820@qq.com>
2025-08-25 17:25:48 +0800

    修复栈问题

--- src/Miscellany/r.cpp
+++ src/Miscellany/r.cpp
@@ -8,7 +8,8 @@ int main(int argc, char* argv[]) {
     string out = s + ".out";
     string err = s + ".err";
 
-    string cp = "g++ -std=c++20 -O2 " + cpp + " -o " + exe;
+    string cp =
+        "g++ -std=c++20 -O2 -Wl,--stack,536870912 " + cpp + " -o " + exe;// 设置栈为 512 MB
 
     if (system(cp.c_str()) != 0) {
         cerr << "Compile Failed!" << endl;
@@ -36,8 +37,3 @@ int main(int argc, char* argv[]) {
 
     return 0;
 }
-/*
-首先编译运行脚本，这只需要执行一次: g++ r.cpp -o r.exe
-以文件内容作为输入: r a a.in (文件名可以随意指定)
-手动输入: r a
-*/
